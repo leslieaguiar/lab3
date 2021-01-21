04de4b87fe31c4fef1e4f6b137f417a7154f1826
Leslie <leaguiar@ucsd.edu>
2021-01-21 10:11:22 -0800

    Added Click function and toggle

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Redirecting");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {    
+    e.preventDefault();
+	$(this).css("background-color", "#000000");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(containingProject).fadeIn();
+       $(containingProject).fadeOut();
+       $(containingProject).fadeToggle();
+
+    }
 }
